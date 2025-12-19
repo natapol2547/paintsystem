e96c97bab79d72fd4219259e345143687dd5a809
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2025-12-19 14:47:29 +0700

    test: broken push

--- preferences.py
+++ preferences.py
@@ -6,13 +6,13 @@ def addon_package() -> str:
 
 @dataclass
 class PaintSystemPreferences:
-    show_tooltips: bool
-    show_hex_color: bool
-    use_compact_design: bool
-    name_layers_group: bool
-    hide_norm_paint_tips: bool
-    hide_color_attr_tips: bool
-    loading_donations: bool
+    # show_tooltips: bool
+    # show_hex_color: bool
+    # use_compact_design: bool
+    # name_layers_group: bool
+    # hide_norm_paint_tips: bool
+    # hide_color_attr_tips: bool
+    # loading_donations: bool
 
 def get_preferences(context) -> PaintSystemPreferences:
     """Get the Paint System preferences"""
