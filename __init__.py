7e08086b665f3a3a557453c183666726909fb4d8
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-07 20:43:35 +0700

    change: change version number

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ bl_info = {
     "author": "Tawan Sunflower, @blastframe",
     "description": "",
     "blender": (4, 2, 0),
-    "version": (2, 1, 8),
+        "version": (2, 1, 8),
     "location": "View3D > Sidebar > Paint System",
     "warning": "",
     "category": "Paint",
