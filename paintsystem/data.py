17a314a02a4fe3f2dec06197cce614482dfa06ef
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-17 16:44:22 +0700

    fix: Adjustment layer in passthrough folder does not work

--- paintsystem/data.py
+++ paintsystem/data.py
@@ -1539,9 +1539,9 @@ class Layer(BaseNestedListItem):
         # If no layer below
         if not below_layer or active_channel.get_parent_layer_id(below_layer, ignore_passthrough=True) != active_channel.get_parent_layer_id(self, ignore_passthrough=True):
             if blend_mode != 'MIX':
-                warnings.append("Blend mode is not MIX. Use Folder with Passthrough blend mode or move the layer")
+                warnings.append("Blend mode is not MIX. Please move the layer or use folder passthrough blend mode")
             if layer_data.type == "ADJUSTMENT":
-                warnings.append("Adjustment disabled. Use Folder with Passthrough blend mode or move the layer")
+                warnings.append("Adjustment disabled. Please move the layer or use folder passthrough blend mode")
             
         return warnings
     
