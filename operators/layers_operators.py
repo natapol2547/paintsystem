7b50b3f2d78098f42bc2f78adbe8a17a64414d2d
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-18 01:29:07 +0700

    feat: move camera if reset projection is executed while in camera view

--- operators/layers_operators.py
+++ operators/layers_operators.py
@@ -1067,11 +1067,27 @@ class PAINTSYSTEM_OT_ProjectionViewReset(PSContextMixin, Operator):
                 location = mathutils.Vector(active_layer.projection_position)
                 rotation = mathutils.Euler(active_layer.projection_rotation, 'XYZ')
                 scale = mathutils.Vector((1.0, 1.0, 1.0))
-                view_matrix = mathutils.Matrix.LocRotScale(location, rotation, scale)
-                if active_layer.projection_space == "OBJECT":
-                    view_matrix = ps_ctx.ps_object.matrix_world @ view_matrix
-                view_matrix.invert()
-                region_3d.view_matrix = view_matrix
+                
+                active_space = context.area.spaces.active
+                if active_space.type == 'VIEW_3D':
+                    region_3d = active_space.region_3d
+                    if region_3d:
+                        match region_3d.view_perspective:
+                            case 'PERSP':
+                                view_matrix = mathutils.Matrix.LocRotScale(location, rotation, scale)
+                                if active_layer.projection_space == "OBJECT":
+                                    view_matrix = ps_ctx.ps_object.matrix_world @ view_matrix
+                                view_matrix.invert()
+                                region_3d.view_matrix = view_matrix
+                            case "CAMERA":
+                                # Set active camera position and rotation 
+                                active_camera = bpy.context.scene.camera
+                                active_camera.location = location
+                                active_camera.rotation_euler = rotation
+                            case _:
+                                self.report({'WARNING'}, "This view perspective is not supported")
+                                return {'CANCELLED'}
+                        return {'FINISHED'}
         return {'FINISHED'}
 
 class PAINTSYSTEM_OT_RenameLayerSuffix(PSContextMixin, Operator):
