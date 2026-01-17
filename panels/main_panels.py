20a9ed84168e97a3b156b67d9b01039c176eac76
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-17 16:17:06 +0700

    feat: channel templates (normal / roughness / metallic channel), Dissolve Paint System node when deleting Paint System from material

--- panels/main_panels.py
+++ panels/main_panels.py
@@ -194,16 +194,6 @@ class MAT_PT_PaintSystemMaterialSettings(PSContextMixin, Panel):
             row.operator("paint_system.new_group", icon='ADD', text="")
             row.operator("wm.call_menu", text="", icon="REMOVE").name = "MAT_MT_DeleteGroupMenu"
 
-class MAT_MT_PaintSystemMaterialSettingsMenu(PSContextMixin, Menu):
-    bl_label = "Material Settings Menu"
-    bl_idname = "MAT_MT_PaintSystemMaterialSettingsMenu"
-
-    def draw(self, context):
-        layout = self.layout
-        col = layout.column(align=True)
-        col.operator("paint_system.new_group", icon='ADD', text="Add Group")
-        col.operator("paint_system.delete_group", icon='REMOVE', text="Delete Group")
-
 class MAT_PT_PaintSystemMainPanel(PSContextMixin, Panel):
     bl_idname = 'MAT_PT_PaintSystemMainPanel'
     bl_space_type = "VIEW_3D"
@@ -216,10 +206,8 @@ class MAT_PT_PaintSystemMainPanel(PSContextMixin, Panel):
         ps_ctx = self.parse_context(context)
         row = layout.row(align=True)
         if ps_ctx.ps_mat_data and ps_ctx.ps_mat_data.groups:
-            # row.operator("wm.call_menu", text="", icon="PREFERENCES").name = "MAT_MT_PaintSystemMaterialSettingsMenu"
-            # row.menu("MAT_MT_PaintSystemMaterialSettingsMenu", text="", icon="PREFERENCES")
             row.operator("paint_system.new_group", icon='ADD', text="")
-            row.operator("paint_system.delete_group", icon='REMOVE', text="")
+            row.operator("wm.call_menu", text="", icon="REMOVE").name = "MAT_MT_DeleteGroupMenu"
         else:
             row.popover("MAT_PT_Support", icon="FUND", text="Wah!")
     
@@ -329,7 +317,10 @@ class MAT_MT_DeleteGroupMenu(PSContextMixin, Menu):
     def draw(self, context):
         layout = self.layout
         ps_ctx = self.parse_context(context)
-        ensure_invoke_context(layout)
+        
+        if layout.operator_context == 'EXEC_REGION_WIN':
+            layout.operator_context = 'INVOKE_REGION_WIN'
+        layout.operator_context = 'INVOKE_REGION_WIN'
         
         layout.alert = True
         layout.operator("paint_system.delete_group", text="Remove Paint System", icon="TRASH")
@@ -340,7 +331,6 @@ classes = (
     MAT_PT_PaintSystemMaterialSettings,
     MATERIAL_UL_PaintSystemGroups,
     MAT_MT_PaintSystemMaterialSelectMenu,
-    MAT_MT_PaintSystemMaterialSettingsMenu,
     MAT_PT_PaintSystemMainPanel,
     MAT_PT_PaintSystemGroups,
     MAT_MT_DeleteGroupMenu,
