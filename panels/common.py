47937523dc4b50359b43465df104ae366aa484b4
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-18 00:07:19 +0700

    feat: Open Image Editor / Shader Editor Operators

--- panels/common.py
+++ panels/common.py
@@ -1,11 +1,9 @@
 import bpy
-from bpy.types import Image, ImagePreview
-import numpy as np
 
 from ..utils.version import is_newer_than
 
 # --
-from ..paintsystem.data import Channel, Layer
+from ..paintsystem.data import Channel
 from ..paintsystem.context import PSContextMixin
 from ..custom_icons import get_icon, get_icon_from_socket_type
 from ..preferences import get_preferences
