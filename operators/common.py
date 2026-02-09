36f8a25b5a918f62e4a4d8cf610ce7323bfc7611
PinkNinjaa <169002704+PinkNinjaa@users.noreply.github.com>
2026-02-09 15:56:18 +1300

    Removed AutoUV coord type
    consolidated with UV type in layers settings. Popovers to choose auto uv stays the same

--- operators/common.py
+++ operators/common.py
@@ -172,14 +172,15 @@ class PSUVOptionsMixin():
         ps_ctx = PSContextMixin.parse_context(context)
         row = layout.row(align=True)
         row.label(text="Coordinate System", icon_value=get_icon('transform'))
-        row.prop(self, "use_paint_system_uv", text="Use AUTO UV?", toggle =1)
+        row.prop(self, "use_paint_system_uv", text="Auto UV", toggle=1)
         if self.use_paint_system_uv:
             info_box = layout.box()
             if not ps_ctx.ps_object.data.uv_layers.get(DEFAULT_PS_UV_MAP_NAME):
                 info_box.alert = True
-                info_box.label(text="Will create a new UV Map: " + DEFAULT_PS_UV_MAP_NAME, icon='ERROR')
+                info_box.label(text="UV: " + DEFAULT_PS_UV_MAP_NAME, icon='ERROR')
+                info_box.label(text="A new UV map will be created", icon='INFO')
             else:
-                info_box.label(text="Using UV Map: " + DEFAULT_PS_UV_MAP_NAME, icon='INFO')
+                info_box.label(text="UV: " + DEFAULT_PS_UV_MAP_NAME, icon='INFO')
             return
         layout.prop(self, "coord_type", text="")
         if self.coord_type != 'UV':
