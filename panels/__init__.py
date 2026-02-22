86d476b3c0f2bc8930f0df1ce1907509c79b1daa
PinkNinjaa <pinkninja.wears.pink@gmail.com>
2026-02-22 18:50:50 +1300

    fix: support bforartists submodule registration and icon cleanup

--- panels/__init__.py
+++ panels/__init__.py
@@ -1,4 +1,5 @@
 import bpy
+from importlib import import_module
 from bpy.utils import register_submodule_factory
 
 submodules = [
@@ -10,5 +11,8 @@ submodules = [
     "layers_panels",
     "quick_tools_panels",
 ]
+
+for submodule in submodules:
+    globals()[submodule] = import_module(f".{submodule}", __name__)
 
 register, unregister = register_submodule_factory(__name__, submodules)
