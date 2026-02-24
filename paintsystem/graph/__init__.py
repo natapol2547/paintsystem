5e53e98316c5156b6030befa25ef00a4042fc63a
PinkNinjaa <pinkninja.wears.pink@gmail.com>
2026-02-25 12:12:19 +1300

    Revert "fix: export create_layer_graph from graph package"
    This reverts commit 2b368640340ecb4e6c8d8d622b60e0004aef76e0.

--- paintsystem/graph/__init__.py
+++ paintsystem/graph/__init__.py
@@ -1,7 +1,2 @@
 from .nodetree_builder import NodeTreeBuilder, START, END, register, unregister, Add_Node
-from .basic_layers import PSNodeTreeBuilder, get_alpha_over_nodetree
-from .common import get_layer_blend_type, set_layer_blend_type
-
-
-def create_layer_graph(layer, context):
-	return PSNodeTreeBuilder.create_layer_graph(layer, context)
+from .basic_layers import *
